8e50b2a03777b1f1c4a002a3cf78e90f40d0600c
Adm-2005 <akshatdmishra2005@gmail.com>
2024-04-29 23:15:50 +0530

    added pickle files and recommendation function

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+import pickle
+from flask import Flask, render_template
+
+def recommend(track_name, similarity_score, df, top_n=5):
+    data = []
+    track_indices = df[df['track_name'] == track_name].index
+    track_idx = track_indices[0] if len(track_indices) > 0 else None
+    if track_idx is not None:
+        sim_scores = list(enumerate(similarity_score[track_idx]))
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        # Here we exclude the track itself, since it will have similarity score of 1 and will be at index 0
+        sim_scores = sim_scores[1:top_n+1] 
+        track_indices = [x[0] for x in sim_scores]
+        for index in track_indices:
+            item = [
+                df.loc[index, 'track_name'],
+                df.loc[index, 'artists'],
+                df.loc[index, 'album_name'],
+                df.loc[index, 'track_genre']
+            ]
+            data.append(item)
+    return data
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
